95cddfea0414854888ff4bcd21a8bfbf336d7b4d
conradbooker <conrad.booker@gmail.com>
2025-04-05 17:27:11 -0500

    hi

--- nu-munch/models.swift
+++ nu-munch/models.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+struct User: Hashable, Codable {
+    let name: String
+    let email: String
+    let profilePhoto: String
+    let currentDelivery: Order?
+    let currentOrder
+    let pastDeliv
+}
+
+struct Order: Hashable, Codable {
+    let id: String
+    let status: String
+    let items: [Item]
+    let locationStart: String
+    let locationEnd: String
+    let price: String
+    let deliverer: Int
+    let orderer: Int
+}
