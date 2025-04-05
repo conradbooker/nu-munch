04c7fd395705cc1e2b490119b2c8a08473795df9
conradbooker <conrad.booker@gmail.com>
2025-04-05 16:33:51 -0500

    Initial Commit

---
+++ nu-munch/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  nu-munch
+//
+//  Created by Conrad on 4/5/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
