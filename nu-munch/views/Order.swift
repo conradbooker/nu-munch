e930f1cbfceec2ce4a684e25871d1f45e5e9752f
conradbooker <conrad.booker@gmail.com>
2025-04-05 18:15:18 -0500

    content view bar

---
+++ nu-munch/views/Order.swift
@@ -0,0 +1,18 @@
+//
+//  Order.swift
+//  nu-munch
+//
+//  Created by Conrad on 4/5/25.
+//
+
+import SwiftUI
+
+struct Order: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Order()
+}
---
+++ nu-munch/views/components/Header.swift
@@ -0,0 +1,18 @@
+//
+//  Header.swift
+//  nu-munch
+//
+//  Created by Conrad on 4/5/25.
+//
+
+import SwiftUI
+
+struct Header: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Header()
+}
