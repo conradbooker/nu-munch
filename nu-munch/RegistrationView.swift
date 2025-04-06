8c3f45a214a879a3b1b47a2b70ba9cc5975ec89c
Daniel Wu <danielwu.a380@gmail.com>
2025-04-05 21:42:08 -0500

    jhjjjkkjnnkjnkjkj

---
+++ nu-munch/RegistrationView.swift
@@ -0,0 +1,79 @@
+//
+//  RegistrationView.swift
+//  nu-munch
+//
+//  Created by Daniel Wu on 4/5/25.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    @State private var email = ""
+    @State private var fullname = ""
+    @State private var password = ""
+    @State private var confirmPassword = ""
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack (spacing: 24) {
+                InputView(text: $email,
+                          title: "Email Address",
+                          placeholder: "name@example.com")
+                .autocapitalization(.none)
+                
+                InputView(text: $fullname,
+                          title: "Full Name",
+                          placeholder: "Enter your name")
+                
+                
+                InputView(text: $password,
+                          title: "Password",
+                          placeholder: "Enter your password",
+                          isSecureField: true)
+                
+                InputView(text: $confirmPassword,
+                          title: "Confirm Password",
+                          placeholder: "Confirm your password",
+                          isSecureField: true)
+            }
+            .padding(.horizontal)
+            .padding(.top, 12)
+            
+            Button {
+                print("Sign user up... ")
+            } label: {
+                HStack {
+                    Text("SIGN UP")
+                        .fontWeight(.semibold)
+                    Image(systemName: "arrow.right")
+                }
+                .foregroundColor(.white)
+                .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+            }
+            .background(Color(.systemPurple))
+            .cornerRadius(8)
+            .padding(.top, 24)
+            
+            Spacer()
+            
+            Button {
+                dismiss()
+            } label: {
+                HStack(spacing: 3) {
+                    Text("Already have an account?")
+                    Text("Sign in")
+                        .fontWeight(.bold)
+                }
+                .font(.system(size:14))
+            }
+        }
+    }
+}
+
+struct RegistrationView_Previews: PreviewProvider{
+    static var previews: some View {
+        RegistrationView()
+    }
+}
